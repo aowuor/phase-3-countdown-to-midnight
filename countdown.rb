94d1bcea635c975ad6f88623a6c5cedd9bbcf66b
aowuor <angelaowuor@gmail.com>
2023-02-11 18:17:36 +0300

    commit tree 00823c4786ac4cf6aebd87777ba2f5947dd72949

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
 #write your code here
 
-def countdown
+def countdown (num)
+    while num > 0
+        puts "#{num} SECOND(S)!"
+        countdown_with_sleep
+        num -= 1
+    end
+    return "HAPPY NEW YEAR!"
 end
+ 
+def countdown_with_sleep(sec = 3)
+    sleep (sec)
+end
+countdown(5)
